d7efb1e61f32a044fd5c358ed2bb5dc7245aca7a
NEW_GITHUB_USERNAME <toniaroyce@gmail.com>
2025-12-15 15:48:51 +0100

    fixed everthing now

--- server.ts
+++ server.ts
@@ -13,12 +13,8 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-);
+app.use(cors());
+app.options("*", cors())
 
 
 setupSwagger(app);
